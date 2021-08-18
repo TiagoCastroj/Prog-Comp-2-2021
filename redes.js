6eedd52b7351847861e76c101c0c0eb9f144cd3d
Tiago Castro <tiaguinhojosecastro@gmail.com>
2021-08-18 20:27:39 -0300

    18-08

--- redes.js
+++ redes.js
@@ -1,6 +1,6 @@
 let gerenciaRedessociais = () => {
     let vetorRede = []
-    for (let i= 0; i<5; i++){
+    for (let i = 0; i < 5; i++) {
         let objeto = {
             codigo: Number(prompt('Qual o codigo da RS?')),
             Nome: prompt('Qual o nome?'),
@@ -11,7 +11,7 @@ let gerenciaRedessociais = () => {
     }
     //cadastro users
     let vetorUser = []
-    for (let i= 0; i<5; i++){
+    for (let i = 0; i < 5; i++) {
         let usuario = {
             login: prompt(`Qual o seu login usuario`),
             nome: prompt(`Qual o nome do usuario?`),
@@ -20,26 +20,50 @@ let gerenciaRedessociais = () => {
         }
         let achou = false // nao encontrou a rs
         let j = 0 // indice do vetor de rede
-        while(!achou && j<5){
-            if (vetorRede[j].codigo == objeto.CodigoRede){
+        while (!achou && j < 5) {
+            if (vetorRede[j].codigo == objeto.CodigoRede) {
                 vetorUser.push(usuario)
                 achou = true
                 alert(`Usuario inserido com sucesso`)
             }
             j++
         }
-        if (!achou){
+        if (!achou) {
             alert(`Rede social nao encontrada`)
         }
-    
-    }
-// Usuario informa a rede social e o programa retorna qts posts foram feitos
-// o programa retorna quanto posts foram feitos em todas as redes cosiais
-// o usuario informa o login do usuario e o programa retorna quantos posts ele fez
-//  o programa retorna quantos posts foram feitos por cada usuário
+        let numero = Number(prompt('informe o cod da rede social'))
+        //percorre vetor
+        let conta = 0
+        for (let i = 0; i < 5; i++) // vetor user
+        {
+            if (vetorUser[i].codigo == conta) {
+                conta = conta + vetorUser[i].QtdePosts
+            }
+        }
+        if (conta = 0) {
+            console.log(`Nao houve postagem ou a rede social nao existe`)
+        }
+        else {
+            console.log(`A qtd de post na rede social ${codigo} foi ${conta}`)
+        }
+        for (let i = 0; i < 5; i++) {
+            let conta = 0
+            for (let j = 0; j < 5; j++){
+                if (vetorRede[i].codigo == vetorUser[j].CodigoRede{
+                    conta = conta + vetorUser[i].QtdePosts
+                }
+            }
+            console.log(`A qtd de posts de uma rede social  ${vetorRede[i].codigo} é ${conta}`)
+        }
 
-    
-    
+    }
+    // Usuario informa a rede social e o programa retorna qts posts foram feitos
+    // o programa retorna quanto posts foram feitos em todas as redes cosiais
+    // o usuario informa o login do usuario e o programa retorna quantos posts ele fez
+    //  o programa retorna quantos posts foram feitos por cada usuário
+
+
+
 
 }
 
